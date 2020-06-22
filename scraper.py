2ea85dd60658d905a53e3dc208930fad0b1b18f0
Shanel Huang <shanelhuang@gmail.com>
2020-06-21 23:35:05 -0700

    update csv file by date, couldn't confirm working

--- scraper.py
+++ scraper.py
@@ -4,6 +4,9 @@ import urllib.request
 import time
 from bs4 import BeautifulSoup
 import csv
+import os.path
+from os import path
+from datetime import date
 
 cascades_url = "https://www.equityapartments.com/seattle/south-lake-union/cascade-apartments##bedroom-type-section-1"
 mark8th_url = "https://www.equityapartments.com/seattle/south-lake-union/mark-on-8th-apartments##bedroom-type-section-1"
@@ -14,9 +17,13 @@ buildings = {
 }
 
 # Export to csv setup
-filename = "aptPrices.csv"
-header = ("Building", "Floor", "Sqft", "Price", "Availability")
+today = date.today()
+formated_date = today.strftime("%m/%d")
+header = ("Building", "Floor", "Sqft",
+          "Availability", "Price " + formated_date)
 data = []
+option = "write_new"
+filename = "aptPrices.csv"
 
 
 def parseHTML(buidling_name, building_url):
@@ -43,31 +50,70 @@ def parseHTML(buidling_name, building_url):
         baths = row_text[bath_start:bath_start+1]
         if beds != '1' or baths != '1':
             continue
-        # print("row_text = " + str(row_text))
-        print("pricing = " + str(pricing))
-        print("time period = " + str(time_period))
-        print("availability = " + str(availability))
-        print("sq ft = " + sq_ft)
-        print("floor = " + floor)
-        print("beds = " + beds)
-        print("baths = " + baths)
-        # print(row)
-        print("------------------------------------------------------------------------------------")
-        data.append((buidling_name, floor, sq_ft, pricing, availability))
+        # printUnits(pricing, time_period, availability, sq_ft, floor, beds, baths)
+        data.append((buidling_name, floor, sq_ft, availability, pricing))
 
 
-def writer(header, data, filename):
+def printUnits(pricing, time_period, availability, sq_ft, floor, beds, baths):
+    # print("row_text = " + str(row_text))
+    print("pricing = " + str(pricing))
+    print("time period = " + str(time_period))
+    print("availability = " + str(availability))
+    print("sq ft = " + sq_ft)
+    print("floor = " + floor)
+    print("beds = " + beds)
+    print("baths = " + baths)
+    # print(row)
+    print("------------------------------------------------------------------------------------")
+
+
+def writer(header, data, filename, option):
     with open(filename, "w", newline="") as csvfile:
-        apt_prices = csv.writer(csvfile)
-        apt_prices.writerow(header)
+        if option == "write":
+            apt_prices = csv.writer(csvfile)
+            apt_prices.writerow(header)
+            for unit in data:
+                apt_prices.writerow(unit)
+        elif option == "update":
+            writer = csv.DictWriter(csvfile, fieldnames=header)
+            writer.writeheader()
+            writer.writerows(data)
+        else:
+            print("csv option not found")
+
+
+def updater(filename):
+    with open(filename, newline="") as file:
+        readData = [row for row in csv.DictReader(file)]
+        readData[0]["Price " + formated_date] = ""
         for unit in data:
-            apt_prices.writerow(unit)
+            if alreadyAdded(unit, readData):
+                continue
+            else:
+                readData.append(unit)
+    readHeader = readData[0].keys()
+    writer(readHeader, readData, filename, "update")
+
+
+# if the unit already exists in the csv file, add the newest price to the
+# existing row and return true, otherwise return false
+def alreadyAdded(unit, readData):
+    for row in readData:
+        if (row.get("Building") == unit[0] and
+            row.get("Floor") == unit[1] and
+            row.get("Sqft") == unit[2] and
+                row.get("Availability") == unit[3]):
+            row['Price ' + formated_date] = unit[4]
+            # print("already added!")
+            return True
+    return False
 
 
 def main():
     for building in buildings:
         parseHTML(building, buildings[building])
-    writer(header, data, filename)
+    writer(header, data, filename, "write")
+    updater(filename)
 
 
 if __name__ == "__main__":
